f0419b1b0b589a14862ac6bff440302eb76cda02
Simon Revill <simon.revill@vertumotors.com>
2024-04-13 23:08:01 +0100

    refactor tests: remove duplication of ShoppingCart instantiation

--- src/ShoppingCart/ShoppingCart.test.ts
+++ src/ShoppingCart/ShoppingCart.test.ts
@@ -1,26 +1,26 @@
 import ShoppingCart from './ShoppingCart';
 
+let cart: ShoppingCart;
+
+beforeEach(() => {
+  cart = new ShoppingCart();
+});
+
 describe('shopping cart', () => {
   describe('add items to cart', () => {
     it('adds a single items to the cart', () => {
-      const cart = new ShoppingCart();
-
       cart.addItems([[10, 1]]);
 
       expect(cart.items).toEqual([[10, 1]]);
     });
 
     it('adds multiple quantities of a single item to the cart', () => {
-      const cart = new ShoppingCart();
-
       cart.addItems([[10, 2]]);
 
       expect(cart.items).toEqual([[10, 2]]);
     });
 
     it('adds two different items with different quantities to the cart', () => {
-      const cart = new ShoppingCart();
-
       cart.addItems([
         [10, 2],
         [20, 3],
@@ -35,24 +35,18 @@ describe('shopping cart', () => {
 
   describe('calculate gross values', () => {
     it('gets gross value for single item in the cart', () => {
-      const cart = new ShoppingCart();
-
       cart.addItems([[10, 1]]);
 
       expect(cart.totalGrossValue).toBe('£10.00');
     });
 
     it('gets gross value of multiple single items in the cart', () => {
-      const cart = new ShoppingCart();
-
       cart.addItems([[10, 2]]);
 
       expect(cart.totalGrossValue).toBe('£20.00');
     });
 
     it('gets gross value for two different items with different quantities in the cart', () => {
-      const cart = new ShoppingCart();
-
       cart.addItems([
         [10, 2],
         [15, 3],
@@ -62,8 +56,6 @@ describe('shopping cart', () => {
     });
 
     it('gets total gross value with unit prices of varying decimal values', () => {
-      const cart = new ShoppingCart();
-
       cart.addItems([
         [17.09, 4],
         [14.5, 1],
@@ -76,8 +68,6 @@ describe('shopping cart', () => {
 
   describe('calculate net value', () => {
     it('gets total net value', () => {
-      const cart = new ShoppingCart();
-
       cart.addItems([
         [10, 2],
         [15, 3],
