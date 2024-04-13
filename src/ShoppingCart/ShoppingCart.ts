d0326b9780bfbbc3b9b587e87c1354ffd75a4e6b
Simon Revill <simon.revill@vertumotors.com>
2024-04-13 13:43:19 +0100

    fix: remove duplicate method

--- src/ShoppingCart/ShoppingCart.ts
+++ src/ShoppingCart/ShoppingCart.ts
@@ -23,13 +23,6 @@ export default class ShoppingCart {
     }).format(value);
   }
 
-  format(value: number | bigint | string): string {
-    return new Intl.NumberFormat('en-GB', {
-      style: 'currency',
-      currency: 'GBP',
-    }).format(value);
-  }
-
   get total(): string {
     return this.format(this._items[0][0] * this._items[0][1]);
   }
