7b7e71f725d0015892dddb483f2570bc2b7487fc
Simon Revill <simon.revill@vertumotors.com>
2024-04-15 08:32:59 +0100

    refactor: remove getter methods and explicity state public and private methods

--- src/ShoppingCart/ShoppingCart.test.ts
+++ src/ShoppingCart/ShoppingCart.test.ts
@@ -11,13 +11,13 @@ describe('shopping cart', () => {
     it('adds a single items to the cart', () => {
       cart.addItems([[10, 1]]);
 
-      expect(cart.list).toEqual([[10, 1]]);
+      expect(cart.list()).toEqual([[10, 1]]);
     });
 
     it('adds multiple quantities of a single item to the cart', () => {
       cart.addItems([[10, 2]]);
 
-      expect(cart.list).toEqual([[10, 2]]);
+      expect(cart.list()).toEqual([[10, 2]]);
     });
 
     it('adds two different items with different quantities to the cart', () => {
@@ -26,7 +26,7 @@ describe('shopping cart', () => {
         [20, 3],
       ]);
 
-      expect(cart.list).toEqual([
+      expect(cart.list()).toEqual([
         [10, 2],
         [20, 3],
       ]);
@@ -35,19 +35,19 @@ describe('shopping cart', () => {
 
   describe('calculate gross values', () => {
     it("gets '£0.00' as gross value if cart is empty", () => {
-      expect(cart.total).toBe('£0.00');
+      expect(cart.total()).toBe('£0.00');
     });
 
     it('gets gross value for single item in the cart', () => {
       cart.addItems([[10, 1]]);
 
-      expect(cart.total).toBe('£10.00');
+      expect(cart.total()).toBe('£10.00');
     });
 
     it('gets gross value of multiple single items in the cart', () => {
       cart.addItems([[10, 2]]);
 
-      expect(cart.total).toBe('£20.00');
+      expect(cart.total()).toBe('£20.00');
     });
 
     it('gets gross value for two different items with different quantities in the cart', () => {
@@ -56,7 +56,7 @@ describe('shopping cart', () => {
         [15, 3],
       ]);
 
-      expect(cart.total).toBe('£65.00');
+      expect(cart.total()).toBe('£65.00');
     });
 
     it('gets total gross value with unit prices of varying decimal values', () => {
@@ -66,13 +66,13 @@ describe('shopping cart', () => {
         [7.22, 2],
       ]);
 
-      expect(cart.total).toBe('£92.12');
+      expect(cart.total()).toBe('£92.12');
     });
   });
 
   describe('calculate net value', () => {
     it("gets '£0.00' as net value if cart is empty", () => {
-      expect(cart.total).toBe('£0.00');
+      expect(cart.total()).toBe('£0.00');
     });
 
     it('gets total net value', () => {
@@ -81,7 +81,7 @@ describe('shopping cart', () => {
         [15, 3],
       ]);
 
-      expect(cart.total).toBe('£65.00');
+      expect(cart.total()).toBe('£65.00');
     });
 
     it('applies a 5% discount if total gross value is over £100', () => {
@@ -90,7 +90,7 @@ describe('shopping cart', () => {
         [10, 1],
       ]);
 
-      expect(cart.total).toBe('£104.50');
+      expect(cart.total()).toBe('£104.50');
     });
 
     it('applies a 10% discount if total gross value is over £200', () => {
@@ -99,7 +99,7 @@ describe('shopping cart', () => {
         [10, 1],
       ]);
 
-      expect(cart.total).toBe('£189.00');
+      expect(cart.total()).toBe('£189.00');
     });
   });
 });
--- src/ShoppingCart/ShoppingCart.ts
+++ src/ShoppingCart/ShoppingCart.ts
@@ -3,46 +3,46 @@ import { Item, DiscountPercentage } from '../types';
 export default class ShoppingCart {
   private items: Item[] = [];
 
-  get list(): Item[] {
-    return this.items;
-  }
-
-  addItems(items: Item[]) {
+  public addItems(items: Item[]) {
     items.forEach((item: Item) => {
       this.items.push(item);
     });
   }
 
-  format(value: number | bigint | string): string {
+  private format(value: number | bigint | string): string {
     return new Intl.NumberFormat('en-GB', {
       style: 'currency',
       currency: 'GBP',
     }).format(value);
   }
 
-  getGrossItemPrices(): string[] {
+  private getGrossItemPrices(): string[] {
     return this.items.map(([unitPrice, quantity]) => this.format(unitPrice * quantity));
   }
 
-  removeCurrencySymbolFromPrice(price: string) {
+  private removeCurrencySymbolFromPrice(price: string) {
     return parseFloat(price.replace('£', ''));
   }
 
-  getTotalGrossPrice(prices: number[]): number {
+  private getTotalGrossPrice(prices: number[]): number {
     return prices.reduce((previousPrice, currentPrice) => previousPrice + currentPrice);
   }
 
-  applyDiscount(price: number, discountPercentage: DiscountPercentage) {
+  private applyDiscount(price: number, discountPercentage: DiscountPercentage) {
     return this.format(price - price * discountPercentage);
   }
 
-  totalGrossValue(): string {
+  private totalGrossValue(): string {
     const grossPricesAsNumbers = this.getGrossItemPrices().map(this.removeCurrencySymbolFromPrice);
 
     return this.format(this.getTotalGrossPrice(grossPricesAsNumbers));
   }
 
-  get total(): string {
+  public list(): Item[] {
+    return this.items;
+  }
+
+  public total(): string {
     if (this.items.length === 0) {
       return '£0.00';
     }
