c83596856bff1064e4f9efb8454fb1b97bd0f863
Simon Revill <simon.revill@vertumotors.com>
2024-04-26 08:41:52 +0100

    refactor: clean up and simplify buildOutputDirectory method.

--- src/services/FileSystemService/FileSystemService.ts
+++ src/services/FileSystemService/FileSystemService.ts
@@ -6,17 +6,13 @@ export default class FileSystemService implements IFileSystemService {
   rootDirectory: string = process.cwd();
 
   buildOutputDirectory(outputDirectoryName: TReceiptOutputDirectory): TReceiptOutputPath {
-    const receiptsDirectoryName = 'receipts';
-    const receiptsDirectory = path.join(this.rootDirectory, receiptsDirectoryName);
+    const receiptsDirectory = path.join(this.rootDirectory, 'receipts');
 
     if (!fs.existsSync(receiptsDirectory)) {
       fs.mkdirSync(receiptsDirectory);
     }
 
-    const outputDirectory = path.join(
-      this.rootDirectory,
-      `${receiptsDirectoryName}/${outputDirectoryName}`,
-    ) as TReceiptOutputPath;
+    const outputDirectory = path.join(receiptsDirectory, outputDirectoryName) as TReceiptOutputPath;
 
     if (!fs.existsSync(outputDirectory)) {
       fs.mkdirSync(outputDirectory);
