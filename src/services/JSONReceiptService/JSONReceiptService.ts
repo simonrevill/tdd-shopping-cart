0afa62d534ed00599db590cd15feb21c7b63657a
Simon Revill <simon.revill@vertumotors.com>
2024-04-27 02:03:48 +0100

    refactor: destructure parameters in JSONReceiptService create method

--- src/services/JSONReceiptService/JSONReceiptService.ts
+++ src/services/JSONReceiptService/JSONReceiptService.ts
@@ -12,22 +12,22 @@ export default class JSONReceiptService implements IReceiptFormatService {
     this.currencyService = currencyService;
   }
 
-  create(data: TRawReceiptData): string {
+  create({ items, subtotal, total, discount }: TRawReceiptData): string {
     const receiptObject: TReceiptData = {
-      items: data.items.map((item) => ({
-        unitPrice: this.currencyService.format(item.unitPrice),
-        quantity: item.quantity.toString(),
-        grossPrice: this.currencyService.format(item.grossPrice),
+      items: items.map(({ unitPrice, quantity, grossPrice }) => ({
+        unitPrice: this.currencyService.format(unitPrice),
+        quantity: quantity.toString(),
+        grossPrice: this.currencyService.format(grossPrice),
       })),
-      ...(data.discount && {
+      ...(discount && {
         discount: {
-          percentage: (data.discount.percentage as number) * 100 + '%',
-          deductedAmount: '-' + this.currencyService.format(data.discount.deductedAmount),
-          netPrice: this.currencyService.format(data.discount.netPrice),
+          percentage: (discount.percentage as number) * 100 + '%',
+          deductedAmount: '-' + this.currencyService.format(discount.deductedAmount),
+          netPrice: this.currencyService.format(discount.netPrice),
         },
       }),
-      subtotal: this.currencyService.format(data.subtotal),
-      total: this.currencyService.format(data.total),
+      subtotal: this.currencyService.format(subtotal),
+      total: this.currencyService.format(total),
     };
 
     const receiptJSON = JSON.stringify(receiptObject);
