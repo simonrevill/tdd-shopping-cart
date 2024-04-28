e16bdeb775a90d0bc644da73107194f14cae0d3d
Simon Revill <simon.revill@vertumotors.com>
2024-04-28 18:20:20 +0100

    refactor: add missing line break in types file

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,6 +5,7 @@ export type TDiscountPercentage = 0.05 | 0.1;
 export type TCurrencyFormatter = (value: number | bigint | string) => string;
 
 export type TNumberFormatter = TCurrencyFormatter;
+
 export interface ICurrencyServiceOptions {
   locale: string;
   currency: string;
