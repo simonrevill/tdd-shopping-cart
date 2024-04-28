898e87bcfcadd52a86ab4d12a1036f760c3e3cc4
Simon Revill <simon.revill@vertumotors.com>
2024-04-28 21:09:58 +0100

    refactor: set test folder location in jest config

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['test'],
 };
 
 export default config;
