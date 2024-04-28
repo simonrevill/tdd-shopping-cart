5f8392ea680cd98b27f9ccc51933e638b32f51d2
Simon Revill <simon.revill@vertumotors.com>
2024-04-28 21:24:46 +0100

    refactor: undo setting the test folder location in jest config - was messing with mutation test results

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,6 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['test'],
 };
 
 export default config;
