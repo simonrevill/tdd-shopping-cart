d510baa347243176892dbcd8897144744ba1fcfb
Simon Revill <simon.revill@vertumotors.com>
2024-04-28 20:37:40 +0100

    refactor: move format instantiations to field

--- src/services/CurrencyService/CurrencyService.ts
+++ src/services/CurrencyService/CurrencyService.ts
@@ -4,31 +4,30 @@ export default class CurrencyService implements ICurrencyService {
   locale: string;
   currency: string;
   currencySymbol: string;
+  currencyFormatter: Intl.NumberFormat;
+  numberFormatter: Intl.NumberFormat;
 
   constructor(locale: string, currency: string, currencySymbol: string) {
     this.locale = locale;
     this.currency = currency;
     this.currencySymbol = currencySymbol;
+
+    this.currencyFormatter = new Intl.NumberFormat(this.locale, {
+      style: 'currency',
+      currency: this.currency,
+    });
+
+    this.numberFormatter = new Intl.NumberFormat(this.locale, {
+      minimumFractionDigits: 2,
+    });
   }
 
   format(value: number | bigint | string): string {
-    const options: Intl.NumberFormatOptions = {
-      style: 'currency',
-      currency: this.currency,
-    };
-
-    const currencyFormat = new Intl.NumberFormat(this.locale, options);
-
-    return currencyFormat.format(value);
+    return this.currencyFormatter.format(value);
   }
 
   formatNumber(value: number | bigint | string): string {
-    const options: Intl.NumberFormatOptions = {
-      minimumFractionDigits: 2,
-    };
-    const numberFormat = new Intl.NumberFormat(this.locale, options);
-
-    return numberFormat.format(value);
+    return this.numberFormatter.format(value);
   }
 
   getZeroPriceInCurrency(): string {
